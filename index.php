29f7ee2b6361850690aa0a8ade384c7784982475
genagggg <mr.genag@mail.ru>
2022-08-18 13:54:31 +0500

    while loop, do...while loop, for loop

--- index.php
+++ index.php
@@ -7,12 +7,29 @@
     <title>Document</title>
 </head>
 <body>
+    <h1 style = "<?php $stl="color:red";echo $stl; ?> "> Heading </h1>
 <?php
-$i = 1;
-while($i<=15){
-    echo "<h1 class=\"heading-item\">Hello world</h1>";
-    $i++;
+//$i = 1;
+//while($i<=15){
+    //echo "<h1 class=\"heading-item\">Hello world</h1>";
+    //$i++;
+//}
+//==========================================================
+
+// $i = 5;
+// do { 
+//     echo "The number is ".$i. "</br>";
+//     $i++;
+// }
+// while($i<=8);
+
+//==============================================
+//The for loop
+
+for($i=0;$i<=10;$i++){
+    echo "Value of i: ".$i."</br>";
 }
+
 ?>
 <script src="script.js"></script>
 </body>
