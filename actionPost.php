e528d5e941e5464e452ba546fdb07a25b6b799fc
genagggg <mr.genag@mail.ru>
2022-10-17 19:58:19 +0500

    Добавление удаление файлов метод POST GET

--- actionPost.php
+++ actionPost.php
@@ -1,6 +1,9 @@
 <?php
-session_start();
-echo $_POST['name'].'   '.$_SESSION['color'];
+if(isset($_POST["text"])){
 echo $_POST['age'];
-
+echo $_POST["text"];
+$myFile=fopen("text.txt","w") or die("Не удалось открыть фаил");
+fwrite($myFile,$_POST["text"]);
+fclose($myFile);
+}
 ?>
