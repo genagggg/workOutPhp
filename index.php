6d356aa2b795a87b5bf92e10c19b6e34b42b0b93
genagggg <mr.genag@mail.ru>
2022-10-18 16:40:48 +0500

    Убрать последнюю запятую при выводе массива строк, при чтении файла

--- index.php
+++ index.php
@@ -11,9 +11,21 @@
 <?php
 
 $read = file('text.txt');
+$count = count($read);
+$i = 1;
 foreach($read as $line){
     echo $line;
+    if($i<$count){
+        echo ', ';
+    }
+    $i++;
 }
+
+    // $anyfile = file('actionPost.php');
+    // foreach($anyfile as $any){
+    //     echo $any;
+    // }
+
 ?>
 
 <script src="script.js"></script>
