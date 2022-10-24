6f4d41f875f44aa60be0647d2c098ee15f0ebf88
genagggg <mr.genag@mail.ru>
2022-10-24 21:06:54 +0500

    OOP PHP inherit

--- index.php
+++ index.php
@@ -9,19 +9,63 @@
 <body>
   
 <?php
-class Person{
-    public $name;
+// class Dog{
+//     public $legs=4;
+//     public function display(){
+//         echo $this->legs;
+//     }
+// }
 
-    public function speak(){
-        echo "Hi";
+// $dog1 = new Dog();
+// $dog1->display();
+
+// $dog2 = new Dog();
+// $dog2->legs=2;
+// $dog2->display();
+
+// class Person{
+//     public function __constructor(){
+//         echo "Object created";
+//     }
+// }
+// $p = new Person();
+// $p->__constructor();
+
+// class Person{
+//     public $name;
+//     public $age;
+//     public function __construct($name, $age){
+//         $this->name=$name;
+//         $this->age=$age;
+//         echo 2;
+//     }
+//     public function __destruct(){
+//         echo 1;
+//     }
+
+// }
+
+// $test = new Person('Жора', 22);
+// $test->__construct();
+// unset($test);
+// 
+class Animals {
+    public $tail;
+    public $age;
+    public function __construct($tail, $age){
+        $this->tail=$tail;
+        $this->age=$age;
     }
+
 }
 
-$person1 = new Person();
-$person1->name="Grigoriy";
-$person1->speak();
-echo $person1->name;
+class Dog extends Animals{}
+
+$dog1 = new Dog(false, 12);
+echo $dog1->tail.'  '.$dog1->age;
+
 ?>
+
 
 <script src="script.js"></script>
 </body>
